0bcdaf98abaf54533acd0d7bdd0214803b1f15d1
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2025-03-06 14:08:19 -0600

    Mejorando la aplicacion de gifs

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { MenuOption } from '../../../interfaces/gif-menu-option.interface';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent {
+  menuOptions:MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      subLabel: 'Gifs populares'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      route: '/dashboard/search',
+      subLabel: 'Buscar Gifs'
+    },
+  ]
+}
